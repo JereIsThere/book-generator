d705c9b513a44497d07087080ee7d9722aa6119b
JereIsThere <46347376+JereIsThere@users.noreply.github.com>
2020-03-21 00:10:54 +0100

    set up framework

---
+++ src/UIClass.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class UIClass {
+    private JPanel filePanel;
+    private JPanel reflowPanel;
+    private JPanel optionsPanel;
+    private JTextField tf_filePath_old;
+    private JButton btn_browse_old;
+    private JTextField tf_filePath_new;
+    private JButton btn_browse_new;
+    private JLabel lbl_lines_info;
+    private JTextField tf_numberOfLines;
+    private JLabel lbl_sampleChapterHeader;
+    private JTextField tf_sampleChapterHeader;
+    private JLabel lbl_linesPerChapterHeader;
+    private JTextField tf_linesPerChapterHeader;
+    private JCheckBox rb_generateLineNumbers;
+    private JButton btn_export;
+}
